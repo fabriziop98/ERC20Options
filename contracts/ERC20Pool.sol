a8dfd8e8ee41334564b00aeeaa5437c029b3a9c2
fabriziop98 <fabriziop@live.com.ar>
2022-10-27 14:25:34 -0300

    basic optionTrigger

--- contracts/ERC20Pool.sol
+++ contracts/ERC20Pool.sol
@@ -40,6 +40,11 @@ contract ERC20Pool is Ownable {
         _;
     }
 
+    modifier validAmount(uint256 _amount){
+        require(_amount > 0, 'Amount not valid');
+        _;
+    }
+
     function setOptionTrigger(address _address)
         onlyOwner
         validAddress(_address)
@@ -48,11 +53,18 @@ contract ERC20Pool is Ownable {
         optionTrigger = _address;
     }
 
+    function receiveFee(address _erc20Address, uint256 _amount)
+        validAddress(_erc20Address)
+        validAmount(_amount)
+    external {
+        
+    }
+
     // @notice lock for options protocol to lock erc20 compatible tokens to pool
     function lock(address _erc20Address, uint256 _amount)
         validAddress(_erc20Address)
+        validAmount(_amount)
     external{
-        require(_amount > 0, 'Amount not valid');
         lockedErc20Balance[IERC20(_erc20Address)] += _amount;
 
         emit LockedAmount(_erc20Address, _amount, lockedErc20Balance[IERC20(_erc20Address)]);
@@ -62,9 +74,9 @@ contract ERC20Pool is Ownable {
     function unlock(uint256 _amount, address _erc20Address) external
         onlyOptionContract(msg.sender)
         validAddress(_erc20Address)
+        validAmount(_amount)
     {
         //Check erc20 and unlock balance
-        require(_amount > 0, 'Amount not valid');
         require(lockedErc20Balance[IERC20(_erc20Address)] >= _amount, 'Not enough locked tokens');
         unlockedErc20Balance[IERC20(_erc20Address)] += _amount;
         lockedErc20Balance[IERC20(_erc20Address)] -= _amount;
--- contracts/OptionTrigger.sol
+++ contracts/OptionTrigger.sol
@@ -2,8 +2,108 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+import "./ERC20Pool.sol";
+import "../interfaces/IERC20.sol";
 
 contract OptionTrigger is Ownable {
+    enum State {
+        Active,
+        Exercised,
+        Expired
+    }
+    enum OptionType {
+        Put,
+        Call
+    }
+    struct Option {
+        State state;
+        address creator;
+        address beneficiary;
+        uint256 strike;
+        uint256 amount;
+        uint256 premium;
+        uint256 expiration;
+        //ERC20
+        address paymentToken;
+        address optionToken;
+    }
+    Option[] public options;
+    ERC20Pool private erc20Pool;
 
+    // Events
+    event OptionCreated(address indexed optionId, address holder);
+    event OptionExecuted(address indexed optionId);
+
+    // Modifiers
+    modifier validAddress(address _address) {
+        require(_address != address(0), "Zero address");
+        _;
+    }
+
+    constructor(address _erc20Pool) {
+        erc20Pool = ERC20Pool(_erc20Pool);
+    }
+
+    // Create option
+    function createOption(
+        uint256 strike,
+        uint256 amount,
+        uint256 premium,
+        uint256 period,
+        address paymentToken,
+        address optionToken
+    )   validAddress(paymentToken)
+        validAddress(optionToken) 
+    external returns (uint256 optionID)
+    {
+        require(strike > 0, "Strike is too small");
+        require(amount > 0, "Amount is too small");
+        require(period >= 1 days, "Period is too short");
+        require(period <= 4 weeks, "Period is too long");
+
+        // Create new Option 
+        optionID = options.length;
+        options.push( 
+            Option(
+                State.Active,
+                msg.sender,
+                address(0),
+                strike,
+                amount,
+                premium,
+                block.timestamp + period,
+                paymentToken,
+                optionToken
+            )
+        );
+
+        //TODO: calculate fees and substract it from amount
+
+        // Validate ERC20 amount and transfer it
+        bool transfered = IERC20(optionToken).transferFrom(msg.sender, address(erc20Pool), amount);
+        require(transfered, "Transfer not posible");
+
+        // Lock funds in erc20Pool
+        erc20Pool.lock(optionToken, amount);
+    }
+
+
+    // Transfer optionToken to lock funds in ERC20Pool
+    function transferOptionToken(address optionToken, uint256 amount)
+        internal
+        validAddress(optionToken)
+    {
+        require(amount > 0, "Amount not valid");
+        bool transfered = IERC20(optionToken).transferFrom(
+            msg.sender,
+            address(erc20Pool),
+            amount
+        );
+        require(transfered, "Transfer not posible");
+    }
+
+    // Transfer fee
+    function transferFee(uint256 amount) internal {
+        require(amount > 0, "Amount not valid");
+    }
 }
