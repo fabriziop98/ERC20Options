87d490fe1be1c59c38ea8102348e9a1303529ccd
fabriziop98 <fabriziop@live.com.ar>
2022-11-22 14:46:20 -0300

    remove unused const

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "";
-
-// Replace this private key with your Goerli account private key.
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key.
-// Beware: NEVER put real Ether into testing accounts
-// Esta es la private key de la cuenta que va a terminar pagando el deploy
-const GOERLI_PRIVATE_KEY = "";
-
 // Import and configure dotenv
 require("dotenv").config();
 
